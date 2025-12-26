c2b40d6d3c4c71878d1274ca86223c314b637892
falcaodiogo <falcao.diogo@ua.pt>
2025-12-26 14:52:14 +0000

    Native wind example + clean code

--- geras/app/(tabs)/explore.tsx
+++ geras/app/(tabs)/explore.tsx
@@ -1,26 +1,22 @@
 import { Image } from 'expo-image';
 import { Platform, StyleSheet } from 'react-native';
 
-import { Collapsible } from '@/components/ui/collapsible';
 import { ExternalLink } from '@/components/external-link';
-import ParallaxScrollView from '@/components/parallax-scroll-view';
 import { ThemedText } from '@/components/themed-text';
 import { ThemedView } from '@/components/themed-view';
+import { Collapsible } from '@/components/ui/collapsible';
 import { IconSymbol } from '@/components/ui/icon-symbol';
 import { Fonts } from '@/constants/theme';
 
 export default function TabTwoScreen() {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
-      headerImage={
-        <IconSymbol
-          size={310}
-          color="#808080"
-          name="chevron.left.forwardslash.chevron.right"
-          style={styles.headerImage}
-        />
-      }>
+    <ThemedView>
+      <IconSymbol
+        size={310}
+        color="#808080"
+        name="chevron.left.forwardslash.chevron.right"
+        style={styles.headerImage}
+      />
       <ThemedView style={styles.titleContainer}>
         <ThemedText
           type="title"
@@ -94,7 +90,7 @@ export default function TabTwoScreen() {
           ),
         })}
       </Collapsible>
-    </ParallaxScrollView>
+    </ThemedView>
   );
 }
 
