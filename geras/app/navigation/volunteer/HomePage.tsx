0b16798a2765e95e9eef96e19665e900a3c69fe7
falcaodiogo <falcao.diogo@ua.pt>
2025-12-27 22:06:47 +0000

    Add buttons to error page

--- geras/app/navigation/volunteer/HomePage.tsx
+++ geras/app/navigation/volunteer/HomePage.tsx
@@ -1,12 +1,22 @@
-import { View } from "react-native";
+import { Pressable, View } from "react-native";
 import { ThemedText } from "@/components/ThemedText";
+import { useRouter } from "expo-router";
 
 export default function Home() {
+  const router = useRouter();
+
   return (
-    <View className="flex-1 items-center justify-center">
+    <View className="flex-1 items-center justify-center gap-8">
       <ThemedText type="title" className="text-primary">
         Homepage do Voluntário
       </ThemedText>
+
+      <Pressable
+        onPress={() => router.push("./ErrorPage")}
+        className="p-4 bg-secondary rounded-xl"
+      >
+        <ThemedText type="subtitle">Go to ErrorPage</ThemedText>
+      </Pressable>
     </View>
   );
 }
