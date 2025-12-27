aeaecdfd51264ac5ca0214f92df643af0a04a414
falcaodiogo <falcao.diogo@ua.pt>
2025-12-27 12:07:01 +0000

    renamed HomePage, add important comment for yall

--- geras/app/+not-found.tsx
+++ geras/app/+not-found.tsx
@@ -1,6 +1,8 @@
 import { ThemedText } from "@/components/ThemedText";
 import { View } from "react-native";
 
+// malta, ver aqui a documentação: https://docs.expo.dev/router/error-handling/
+
 export default function NotFound() {
     return (
         <View className="flex-1 items-center justify-center">
--- geras/app/navigation/caretaker/HomePage.tsx
+++ geras/app/navigation/caretaker/HomePage.tsx
@@ -1,7 +1,7 @@
 import { View } from "react-native";
 import { ThemedText } from "@/components/ThemedText";
 
-export default function Home() {
+export default function HomePage() {
   return (
     <View className="flex-1 items-center justify-center">
       <ThemedText type="title" className="text-primary">
