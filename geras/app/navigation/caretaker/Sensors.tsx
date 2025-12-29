38a6d58fd46b49caf2348b780ec03b92550e1621
falcaodiogo <falcao.diogo@ua.pt>
2025-12-29 11:40:21 +0000

    Gustavo changes from conflict

--- geras/app/navigation/caretaker/Sensors.tsx
+++ geras/app/navigation/caretaker/Sensors.tsx
@@ -1,12 +1,10 @@
-import { View } from 'react-native';
+import { useState } from 'react';
+import { View, ScrollView } from 'react-native';
 import { ThemedText } from '@/components/ThemedText';
-import React, { useState } from "react";
-import { View, ScrollView } from "react-native";
-import { ThemedText } from "@/components/ThemedText";
-import SensorComponent from "@/components/caretaker/SensorComponent";
-import AddButtonComponent from "@/components/caretaker/AddButtonComponent";
-import { sensorsData, type Sensor } from "@/data/sensorsData";
-import { SafeAreaView } from "react-native-safe-area-context";
+import SensorComponent from '@/components/caretaker/SensorComponent';
+import AddButtonComponent from '@/components/caretaker/AddButtonComponent';
+import { sensorsData, type Sensor } from '@/data/sensorsData';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function Sensors() {
   const [sensors, setSensors] = useState<Sensor[]>(sensorsData);
@@ -14,20 +12,14 @@ export default function Sensors() {
   const toggleSensor = (id: string) => {
     setSensors((currentSensors) =>
       currentSensors.map((s) =>
-        s.id === id ? { ...s, active: !s.active } : s
-      )
+        s.id === id ? { ...s, active: !s.active } : s,
+      ),
     );
   };
 
   return (
-    <SafeAreaView
-        edges={['top']}
-        className="flex-1 pt-24"
-      > 
-      <ScrollView
-        className="flex-1 px-6"
-        showsVerticalScrollIndicator={false}
-      >
+    <SafeAreaView edges={['top']} className="flex-1 pt-24">
+      <ScrollView className="flex-1 px-6" showsVerticalScrollIndicator={false}>
         <View className="mb-6">
           <ThemedText type="title" className="mb-6">
             Sensores de António Silva
@@ -47,7 +39,7 @@ export default function Sensors() {
         </View>
 
         <AddButtonComponent
-          onPress={() => console.log("Add Sensor Button Pressed")}
+          onPress={() => console.log('Add Sensor Button Pressed')}
           title="Adicionar"
         />
 
