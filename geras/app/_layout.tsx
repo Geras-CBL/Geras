36fa6c62fbf5afc9c741240dca91d93746f4ae07
falcaodiogo <falcao.diogo@ua.pt>
2025-12-30 12:36:12 +0000

    Add RequestHelp screen and button component for senior navigation

--- geras/app/_layout.tsx
+++ geras/app/_layout.tsx
@@ -77,6 +77,22 @@ export default function RootLayout() {
           }}
         />
         <Stack.Screen
+          name="navigation/senior/RequestHelp"
+          options={{
+            headerShown: true,
+            headerTransparent: true,
+            contentStyle: { backgroundColor: '#fbfbfb' },
+            header: () => (
+              <Header
+                leftIconName="home"
+                rightIconName="settings"
+                onLeftPress={() => router.push('/navigation/senior/HomePage')}
+                onRightPress={() => router.push('/navigation/senior/Settings')}
+              />
+            ),
+          }}
+        />
+        <Stack.Screen
           name="navigation/senior/ErrorPage"
           options={{ headerShown: false }}
         />
--- geras/app/navigation/senior/RequestHelp.tsx
+++ geras/app/navigation/senior/RequestHelp.tsx
@@ -1,12 +1,43 @@
-import { View } from 'react-native';
+import BottomActions from '@/components/senior/BottomActions';
+import RequestHelpButton from '@/components/senior/RequestHelpButton';
+import SectionTitle from '@/components/shared/SectionTitle';
 import { ThemedText } from '@/components/ThemedText';
+import { View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function RequestHelp() {
   return (
-    <View className="flex-1 items-center justify-center">
-      <ThemedText type="title" className="text-primary">
-        Pedir Ajuda
-      </ThemedText>
-    </View>
+    <SafeAreaView
+      edges={['top']}
+      className="h-full flex-1 justify-around px-6 pb-56 pt-24"
+    >
+      <SectionTitle title="Pedir Ajuda" />
+      <View className="h-full flex-col justify-around">
+        <ThemedText type="subtitle" style={{ fontSize: 16 }}>
+          Olá Senhor António, o que precisa...
+        </ThemedText>
+        <RequestHelpButton
+          iconName="shopping-cart"
+          label="Compras mercado"
+          route={'../../navigation/senior/Groceries'}
+        />
+        <RequestHelpButton
+          iconName="local-pharmacy"
+          label="Compras farmácia"
+          route={'../../navigation/senior/PharmacyShopping'}
+        />
+        <RequestHelpButton
+          iconName="cleaning-services"
+          label="Tarefas domésticas"
+          route={'../../navigation/senior/HouseHoldTasks'}
+        />
+        <RequestHelpButton
+          iconName="construction"
+          label="Outros pedidos"
+          route={'../../navigation/senior/OtherRequests'}
+        />
+      </View>
+      <BottomActions />
+    </SafeAreaView>
   );
 }
