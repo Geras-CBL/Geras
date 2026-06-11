6f981853550fa939bc907007baa97198c5b9a9ea
Tomás <tomasandrenogueira@gmail.com>
2026-06-11 22:52:22 +0100

    Prettier - Correção Teste Jest

--- geras/components/__tests__/Settings.test.tsx
+++ geras/components/__tests__/Settings.test.tsx
@@ -35,7 +35,7 @@ jest.mock('react-native-safe-area-context', () => ({
 }));
 
 jest.mock('@react-native-async-storage/async-storage', () =>
-  require('@react-native-async-storage/async-storage/jest/async-storage-mock')
+  require('@react-native-async-storage/async-storage/jest/async-storage-mock'),
 );
 
 jest.mock('react-native-health-connect', () => ({
